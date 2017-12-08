31d88dea603adabf3e55f56d9e90629e86dc68db
DougKrioK <douglaspinheiro@id.uff.br>
2017-12-08 06:11:15 -0200

    criação da classe ProdutosDao

---
+++ app/infra/ProdutosDAO.js
@@ -0,0 +1,12 @@
+class ProdutosDAO {
+    constructor(connection) {
+        this._connection = connection;
+    }
+    lista(callback) {
+        this._connection.query('select * from produtos', callback);
+    }
+}
+
+module.exports = function(){
+    return ProdutosDAO;
+}
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
     app.get("/produtos",function(req, res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(connection, function(err, results){
+        produtosBanco.lista(function(err, results){
             
             res.render('produtos/lista', {lista: results});
         });
