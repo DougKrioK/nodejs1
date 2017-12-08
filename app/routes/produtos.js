24a03ffab59d740eb15d7ffc2ee9cb5c7f13a4a8
DougKrioK <douglaspinheiro@id.uff.br>
2017-12-08 13:06:59 -0200

    Exibindo as mensagens de erro para o usuário, com express-validator

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -26,13 +26,32 @@ module.exports = function(app) {
     app.get("/produtos",listarProdutos);
 
     app.get("/produtos/form",function(req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form',
+            {errosValidacao:{}}
+        );
     });
 
     app.post("/produtos",function(req, res) {
 
         //middleware pega a requisição e transforma em json.
         var produto = req.body;
+
+        /**
+         * O Express Validator vai adicionar algumas funções no request, onde podemos acessar através das variáveis req.
+            Após instalar essa biblioteca, vamos no arquivo produtos-js, dentro da função de mapeamento do /produtos. Uma função que é adicionada no request é a req.assert(), onde nós passamos o campo que queremos validar e a mensagem que será associada a nossa validação. req.assert() nos retorna um objeto que representa a validação para o titulo.
+         */
+        req.assert('titulo','Titulo é obrigatório').notEmpty();
+        req.assert('preco','Formato invalido').isFloat();
+
+        //Com o objeto do validator, vamos chamar a função que sinaliza erro caso o valor seja vazio notEmpty().c
+        //validadorTitulo.notEmpty();
+        //Com isso estamos dizendo que se algo estiver errado, não passará. Agora vamos chamar o req.validationErrors(); para verificar se ouve algum erro, e nos retorna um JSON com os erros.
+        var erros = req.validationErrors();
+        //Verificamos se ouve erros com o if(erros), caso a variável erros seja preenchida com algum erro, o JavaScript vai considerar como true. Caso haja erros, renderizamos o formulário novamente e usamos um return; vazio, para que ele saia do método app.post() sem cadastrar.
+        if(erros){
+            res.render('produtos/form',{errosValidacao: erros});
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function(){
     
@@ -8,10 +9,12 @@ module.exports = function(){
 
     app.set('view engine','ejs');
     app.set('views','./app/views');//setando localização das views pro express
-
+    
     app.use(bodyParser.urlencoded({extended: true}));
     //middleware, analisa se é json
     app.use(bodyParser.json());
+
+    app.use(expressValidator());
 
 
     load('routes', { cwd: 'app'})
