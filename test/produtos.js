cdf8d8f6a61ba291d29ab96582844974ee47cfbe
DougKrioK <douglaspinheiro@id.uff.br>
2017-12-08 15:33:47 -0200

    inclui html nos testes do mocha

--- test/produtos.js
+++ test/produtos.js
@@ -1,12 +1,20 @@
 var express = require('../config/express')();
 var request = require('supertest')(express);
 
-describe('ProdutosController',function(){
-    it('listagem json', function(done){
+describe('#ProdutosController',function(){
+    it('#listagem json', function(done){
 
         request.get('/produtos')
             .set('Accept','application/json')
             .expect('Content-Type',/json/)
+            .expect(200,done);
+    });
+
+    it('$listagem html', function(done){
+        
+        request.get('/produtos')
+            .set('Accept','text/html')
+            .expect('Content-Type',/html/)
             .expect(200,done);
     })
 })
