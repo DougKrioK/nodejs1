22f5e12821b9b15a516f8fd64597a12c9614a4e2
DougKrioK <douglaspinheiro@id.uff.br>
2017-12-08 06:56:25 -0200

    Configuração do body-parser

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -5,6 +5,10 @@ class ProdutosDAO {
     lista(callback) {
         this._connection.query('select * from produtos', callback);
     }
+
+    salva(produto, callback) {
+        this._connection.query('insert into produtos set ?',produto, callback);
+    }
 }
 
 module.exports = function(){
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
     app.get("/produtos",function(req, res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function(err, results){
+        produtosDAO.lista(function(err, results){
             
             res.render('produtos/lista', {lista: results});
         });
@@ -13,8 +13,20 @@ module.exports = function(app) {
     });
 
     app.get("/produtos/form",function(req, res) {
-            
         res.render('produtos/form');
+    });
+
+    app.post("/produtos/salva",function(req, res) {
+
+        var produto = req.body;
+        console.log(produto);
+        
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto,function(erros,resultados){
+            res.render('produtos/lista');
+        })
 
     });
 }
